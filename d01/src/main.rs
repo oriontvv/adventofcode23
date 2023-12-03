46c3f0e508610b6a8eb00748fc392d9773adffb2
oriontvv <taranov.vv@gmail.com>
2023-12-03 21:27:28 +0300

    refactoring d01

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -1,53 +1,45 @@
-use std::{collections::HashMap, fs::read_to_string};
+use std::fs::read_to_string;
 
-fn calibration(s: &str) -> usize {
+fn calibration(s: &str) -> Option<usize> {
     let numbers: Vec<_> = s
         .matches(char::is_numeric)
         .map(|c| c.parse::<usize>().unwrap())
         .collect();
-    10 * numbers.first().unwrap() + numbers.last().unwrap()
+    Some(10 * numbers.first()? + numbers.last()?)
 }
 
-fn calibration_2(s: &str, map: &HashMap<&str, usize>) -> usize {
+fn calibration_2(s: &str, words: &Vec<&str>) -> Option<usize> {
     let mut numbers = Vec::<usize>::new();
     for (index, c) in s.chars().enumerate() {
         if c.is_numeric() {
             numbers.push(c.to_string().parse::<usize>().unwrap());
             continue;
         }
-        for (key, value) in map.iter() {
-            if s[index..].starts_with(*key) {
-                numbers.push(*value);
+        for (word_index, word) in words.iter().enumerate() {
+            if s[index..].starts_with(*word) {
+                numbers.push(word_index + 1);
                 break;
             }
         }
     }
-    10 * numbers.first().unwrap() + numbers.last().unwrap()
+    Some(10 * numbers.first()? + numbers.last()?)
 }
 
 fn main() {
     // 1
     // let mut total: usize = 0;
     // for line in read_to_string("input").unwrap().lines() {
-    //     total += calibration(line);
+    //     total += calibration(line).unwrap();
     // }
     // println!("total: {total}");
 
     // 2
-    let map = HashMap::from_iter([
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-    ]);
+    let words = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
     let mut total: usize = 0;
     for line in read_to_string("input").unwrap().lines() {
-        total += calibration_2(line, &map);
+        total += calibration_2(line, &words).unwrap();
     }
     println!("total: {total}");
 }
