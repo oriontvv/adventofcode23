314d3825cbe382ab25a7e1301dbdd78f3bc1ad95
oriontvv <taranov.vv@gmail.com>
2023-12-04 16:49:24 +0300

    d04 done

---
+++ d04/src/main.rs
@@ -0,0 +1,49 @@
+use std::collections::HashSet;
+use std::fs::read_to_string;
+
+fn find_numbers(s: &str) -> HashSet<usize> {
+    s.split(' ')
+        .filter_map(|num| num.trim().parse::<usize>().ok())
+        .collect()
+}
+fn calc_common(s: &str) -> usize {
+    let (win, actual) = s.split_once('|').unwrap();
+    let win = find_numbers(win);
+    let actual = find_numbers(actual);
+    win.intersection(&actual).count()
+}
+
+fn calc_cards(common: usize) -> usize {
+    if common > 0 {
+        1 << (common - 1)
+    } else {
+        0
+    }
+}
+
+fn main() {
+    // 1
+    let mut total: usize = 0;
+    for line in read_to_string("input").unwrap().lines() {
+        let common = calc_common(line);
+        total += calc_cards(common);
+    }
+    println!("total: {total}");
+
+    // 2
+    let cards: Vec<usize> = read_to_string("input")
+        .unwrap()
+        .lines()
+        .map(calc_common)
+        .collect();
+    // start indexing with 1
+    let mut totals = vec![1; cards.len() + 1];
+    totals[0] = 0;
+    for (card_number, &common_cards) in cards.iter().enumerate() {
+        let card_number = card_number + 1;
+        for index in card_number..(card_number + common_cards) {
+            totals[index + 1] += totals[card_number];
+        }
+    }
+    println!("total: {}", totals.iter().sum::<usize>());
+}
