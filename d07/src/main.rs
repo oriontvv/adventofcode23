8ca74ff55cd0535a7b97e1ba6c4766e364298efc
oriontvv <taranov.vv@gmail.com>
2023-12-07 18:08:37 +0300

    refactoring d07

--- d07/src/main.rs
+++ d07/src/main.rs
@@ -26,8 +26,13 @@ fn match_counts(counts: &Vec<usize>) -> HandType {
     }
 }
 
+type Bet = usize;
+
+#[derive(Debug, Clone, PartialEq, Ord, Eq)]
+struct Cards(String);
+
 #[derive(Debug, PartialEq, Ord, Eq)]
-struct Hand(HandType, Cards, usize);
+struct Hand(HandType, Cards, Bet);
 
 impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
@@ -38,79 +43,58 @@ impl PartialOrd for Hand {
     }
 }
 
-// 1
-// #[derive(Debug, PartialEq, Ord, Eq)]
-// struct Cards(String);
-
-// impl PartialOrd for Cards {
-//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-//         let values: Vec<_> = self.0.chars().map(card_value).collect();
-//         let other_values: Vec<_> = other.0.chars().map(card_value).collect();
-//         Some(values.cmp(&other_values))
-//     }
-// }
-// fn card_value(card: char) -> usize {
-//     "23456789TJQKA".find(card).unwrap()
-// }
-
-// impl FromStr for Hand {
-//     type Err = ();
-//     fn from_str(s: &str) -> Result<Self, Self::Err> {
-//         let (cards, bet) = s.split_once(" ").unwrap();
-//         Ok(Self(
-//             detect_hand_type(cards),
-//             Cards(cards.to_string()),
-//             bet.parse().unwrap(),
-//         ))
-//     }
-// }
-
-// fn detect_hand_type(cards: &str) -> HandType {
-//     let counter: Counter<_> = cards.chars().collect();
-//     let mut counts: Vec<_> = counter.values().copied().collect();
-//     counts.sort_by(|a, b| b.cmp(a));
-//     match_counts(&counts)
-// }
-
-// 2
-#[derive(Debug, PartialEq, Ord, Eq)]
-struct Cards(String);
-
-impl PartialOrd for Cards {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        let values: Vec<_> = self.0.chars().map(card_value2).collect();
-        let other_values: Vec<_> = other.0.chars().map(card_value2).collect();
-        Some(values.cmp(&other_values))
-    }
-}
-fn card_value2(card: char) -> usize {
-    "J23456789TQKA".find(card).unwrap()
-}
-
 impl FromStr for Hand {
     type Err = ();
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let (cards, bet) = s.split_once(" ").unwrap();
-        Ok(Self(
-            detect_hand_type2(cards),
-            Cards(cards.to_string()),
-            bet.parse().unwrap(),
-        ))
+        let cards = Cards(cards.to_string());
+        Ok(Self(cards.clone().into(), cards, bet.parse().unwrap()))
     }
 }
 
-fn detect_hand_type2(cards: &str) -> HandType {
-    let cards_without_jokers = cards.replace("J", "");
-    let non_jokers = cards_without_jokers.len();
-    if non_jokers == 0 {
-        return HandType::FiveKind;
+// 1
+// impl PartialOrd for Cards {
+//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+//         let cmp = |card: char| "23456789TJQKA".find(card).unwrap();
+//         let values: Vec<_> = self.0.chars().map(cmp).collect();
+//         let other_values: Vec<_> = other.0.chars().map(cmp).collect();
+//         Some(values.cmp(&other_values))
+//     }
+// }
+
+// impl From<Cards> for HandType {
+//     fn from(value: Cards) -> Self {
+//         let counter: Counter<_> = value.0.chars().collect();
+//         let mut counts: Vec<_> = counter.values().copied().collect();
+//         counts.sort_by(|a, b| b.cmp(a));
+//         match_counts(&counts)
+//     }
+// }
+
+// 2
+impl PartialOrd for Cards {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let cmp = |card: char| "J23456789TQKA".find(card).unwrap();
+        let values: Vec<_> = self.0.chars().map(cmp).collect();
+        let other_values: Vec<_> = other.0.chars().map(cmp).collect();
+        Some(values.cmp(&other_values))
     }
-    let counter: Counter<_> = cards_without_jokers.chars().collect();
-    let mut counts: Vec<_> = counter.values().copied().collect();
-    counts.sort_by(|a, b| b.cmp(a));
-    // add jokers to first combination
-    counts[0] += 5 - non_jokers;
-    match_counts(&counts)
+}
+
+impl From<Cards> for HandType {
+    fn from(value: Cards) -> Self {
+        let cards_without_jokers = value.0.replace("J", "");
+        let non_jokers = cards_without_jokers.len();
+        if non_jokers == 0 {
+            return HandType::FiveKind;
+        }
+        let counter: Counter<_> = cards_without_jokers.chars().collect();
+        let mut counts: Vec<_> = counter.values().copied().collect();
+        counts.sort_by(|a, b| b.cmp(a));
+        // add jokers to first combination
+        counts[0] += 5 - non_jokers;
+        match_counts(&counts)
+    }
 }
 
 fn main() {
